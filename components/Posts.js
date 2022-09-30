990fc9c3f6b73913d72ea01c663c74b9b5189f9e
Hasan Shanto <hasanshanto922@gmail.com>
2022-09-30 20:18:22 +0600

    suggestion

--- components/Posts.js
+++ components/Posts.js
@@ -11,9 +11,37 @@ const posts = [
     },
     {
         id: 1,
+        username: "@Samson-H",
+        userImage: "https://tse2.explicit.bing.net/th?id=OIP.LEAJoBlmc6dzvKL-tCEx1wHaHa&pid=Api&P=0",
+        img: "https://tse2.explicit.bing.net/th?id=OIP.LEAJoBlmc6dzvKL-tCEx1wHaHa&pid=Api&P=0",
+        caption: "Hello Guys! cool dude has come back"
+    },
+    {
+        id: 1,
+        username: "@Commander",
+        userImage: "https://yt3.ggpht.com/a/AATXAJyCblGT8JqdpzvvObORk3c4T_xUBx0d6U1dzA=s900-c-k-c0xffffffff-no-rj-mo",
+        img: "https://yt3.ggpht.com/a/AATXAJyCblGT8JqdpzvvObORk3c4T_xUBx0d6U1dzA=s900-c-k-c0xffffffff-no-rj-mo",
+        caption: "Hello Guys! cool dude has come back"
+    },
+    {
+        id: 1,
         username: "@shanto_0000",
         userImage: "https://avatars.githubusercontent.com/u/97034683?v=4",
         img: "https://avatars.githubusercontent.com/u/97034683?v=4",
+        caption: "Hello Guys! cool dude has come back"
+    },
+    {
+        id: 1,
+        username: "@HasanShanto",
+        userImage: "https://tse2.mm.bing.net/th?id=OIP.cLdz-ArOuQIk5jM3wn9u0wHaHa&pid=Api&P=0",
+        img: "https://tse2.mm.bing.net/th?id=OIP.cLdz-ArOuQIk5jM3wn9u0wHaHa&pid=Api&P=0",
+        caption: "Hello Guys! cool dude has come back"
+    },
+    {
+        id: 1,
+        username: "@Andry_979",
+        userImage: "https://i.pinimg.com/originals/a2/de/39/a2de3954697c636276192afea0a6f661.jpg",
+        img: "https://i.pinimg.com/originals/a2/de/39/a2de3954697c636276192afea0a6f661.jpg",
         caption: "Hello Guys! cool dude has come back"
     }
 ]
--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,8 +1,22 @@
-
+import { useEffect, useState } from "react"
+import {faker} from "@faker-js/faker"
 
 function Suggestions() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      userId: faker.datatype.uuid(),
+    }));
+    setSuggestions(suggestions);
+  },[])
   return (
-    <div>Suggestions</div>
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3>Suggestions for you</h3>
+        <button>See All</button>
+      </div>
+    </div>
   )
 }
 
