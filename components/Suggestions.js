cf427f905ef3f2cfba8b810f1c19576067e74b2d
Hasan Shanto <hasanshanto922@gmail.com>
2022-09-30 20:37:33 +0600

    fake suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -7,15 +7,36 @@ function Suggestions() {
   useEffect(() => {
     const suggestions = [...Array(5)].map((_, i) => ({
       userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+        email: faker.internet.email(),
+        avatar: faker.image.avatar(),
+        password: faker.internet.password(),
+        birthdate: faker.date.birthdate(),
+        registeredAt: faker.date.past(),
     }));
     setSuggestions(suggestions);
   },[])
   return (
     <div className="mt-4 ml-10">
       <div className="flex justify-between text-sm mb-5">
-        <h3>Suggestions for you</h3>
-        <button>See All</button>
+        <h3 className="text-sm font-bold">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
       </div>
+      {
+        suggestions.map(profile => (
+          <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+          >
+            <img className="w-10 h-10 rounded-full border p-[2px]" src={profile.avatar}/>
+          <div className="flex-1 ml-4">
+            <h2 className="font-semibold text-sm">{profile.username}</h2>
+            <h2>Email: {profile.email} </h2>
+          </div>
+          
+          </div>
+        ))
+      }
     </div>
   )
 }
